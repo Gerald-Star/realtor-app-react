0b27590f2d003e43fd0e67e98283a45fccd925ae
Gerald-Star <chimagerald3@gmail.com>
2023-01-12 17:03:29 +0100

    create sign in, sign up, forgot password pages and oauth component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import Offers from "./pages/Offers";
-
-
-
+import Header from "./components/Header";
 
 
 
 function App() {
+
   return (
     <>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
