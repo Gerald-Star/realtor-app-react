9f980bd60bfcd0c8b6e8e3499cb6a55c3c1cf22f
Gerald-Star <chimagerald3@gmail.com>
2023-01-26 16:51:57 +0100

    made the header component to be dynamic for sign in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 // import {Navigate, useLocation} from 'react-router-dom'
 import { useLocation, useNavigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged} from "firebase/auth";
 
 export default function Header() {
+  const [pageState, setPageState] = useState("sign in")
   const location = useLocation();
+  const auth = getAuth();
   const navigate = useNavigate();
+
+  
+  
+  useEffect (() => {
+    onAuthStateChanged(auth, (user)=> {
+      if(user) {
+        setPageState("Profile");
+      }else  {
+        setPageState("sign in");
+      }
+
+    });
+  }, [auth])
+
+
   function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true
     }
   }
-   
+
+  
+
+
+  
+  //for authentication initialize the auth from getAuth
+  // use the useEffect function to check the changes of auth to call a function
+  //each time the auth changes we need to call the auth dependencies
   
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
@@ -46,11 +71,19 @@ export default function Header() {
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 
               border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/sign-in") && "text-black border-b-red-500"
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "text-black border-b-red-500"
               }`}
-              onClick={() => navigate("/sign-in")}
-            >
-              Sign in
+              onClick={() => navigate("/profile")}>
+              {/*Make the sign-in Dynamic by creating a hook for the sign-in at the top.
+               The onclick brings the user to the sign-in screen. 
+               make it dynamic so that when the user is authenticated. 
+               it goes to the profile page.
+               Change the onclick old position from sign-in to profile
+              
+              */}
+
+             { /*Change the Sign in here to pageState to make it dynamic*/}
+             {pageState}
             </li>
             
           </ul>
