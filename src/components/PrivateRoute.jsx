0b3b1b24a8132018dc276f82f5efaf42a34c78ee
Gerald-Star <chimagerald3@gmail.com>
2023-01-26 13:26:02 +0100

    created user profile page and user authentication when login

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,23 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useAuthStatus } from "../hooks/useAuthStatus";
+// import Spinner from "./Spinner";
+export default function PrivateRoute() {
+  const { loggedIn, checkingStatus } = useAuthStatus();
+  if (checkingStatus) {
+    return  <h3>Loading.....</h3>
+  }
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
+}
+
+
+
+
+
+
+
+
+//add private route to component and render it with navigation component in  App.js
+//setting the private route to false will not allow anybody to get to the profile page
+// To set the private route for better login, create a new custom hooks-/ create hooks component
+// create the hook useAuthStatus
+//   const loggedIn = false; import the login and checkingStatus from the useAuthStatus
---
+++ src/hooks/useAuthStatus.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export function useAuthStatus() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    console.log(auth);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setCheckingStatus(false);
+    });
+  }, []);
+  return { loggedIn, checkingStatus };
+}
+
+//this calls the useEffect one time
+//for return, return the logged in user and check in status of the user. If the user is not logged in or not authenticated.
