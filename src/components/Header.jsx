86093beee92b207ae8bcdca7e7efca8626dc8cca
Gerald-Star <chimagerald3@gmail.com>
2023-01-26 17:41:28 +0100

    create a loader spinner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ export default function Header() {
   //each time the auth changes we need to call the auth dependencies
   
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,15 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useAuthStatus } from "../hooks/useAuthStatus";
-// import Spinner from "./Spinner";
+import Spinner from  "./Spinner"
+
+
 export default function PrivateRoute() {
   const { loggedIn, checkingStatus } = useAuthStatus();
   if (checkingStatus) {
-    return  <h3>Loading.....</h3>
+    // return  <h3>Loading.....</h3> change this loading to spinner component
+    //import spinner
+    return <Spinner />
+
   }
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 }
